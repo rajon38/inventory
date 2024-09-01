46e1475dfbff800e9239bb9936ca0e8004892129
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-09-01 21:38:49 +0600

    updated the frontend

---
+++ Frontend/build/static/js/777.2cc1f3c4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient_side=self.webpackChunkclient_side||[]).push([[777],{8692:(e,s,c)=>{c.r(s),c.d(s,{default:()=>i});c(5043);const a=c.p+"static/media/404.2d47255f361c6cf188c4.png";var n=c(579);const i=()=>(0,n.jsx)("div",{className:"center-screen",children:(0,n.jsx)("img",{className:"animated fadeIn",src:a})})}}]);
+//# sourceMappingURL=777.2cc1f3c4.chunk.js.map
